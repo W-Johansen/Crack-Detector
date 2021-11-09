7baba662eb742ac1269dec72fe27c67ca6d8d1d4
William Johansen <william.johansen1999@gmail.com>
2021-11-09 11:28:29 +0100

    Added demo files

---
+++ cameraDemo.py
@@ -0,0 +1,56 @@
+import os
+import cv2
+import numpy as np
+from datetime import date, datetime
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from os import listdir
+from os.path import isfile, join
+
+def get_jetson_gstreamer_source(capture_width=1280, capture_height=720, display_width=1280, display_height=720, framerate=12, flip_method=0):
+    """
+    Return an OpenCV-compatible video source description that uses gstreamer to capture video from the camera on a Jetson Nano
+    """
+    return (
+            f'nvarguscamerasrc ! video/x-raw(memory:NVMM), ' +
+            f'width=(int){capture_width}, height=(int){capture_height}, ' +
+            f'format=(string)NV12, framerate=(fraction){framerate}/1 ! ' +
+            f'nvvidconv flip-method={flip_method} ! ' +
+            f'video/x-raw, width=(int){display_width}, height=(int){display_height}, format=(string)BGRx ! ' +
+            'videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+            )
+
+def predictor3(img):
+    test_image = cv2.resize(img, (128,128))
+    test_image = np.expand_dims(test_image, axis=0)
+    result=model.predict(test_image)
+    #training_set.class_indices
+    if result[0][0] == 1:
+        return False
+    else:
+        return True
+
+model = load_model('models/model-crop.h5')
+
+model.summary()
+
+video_capture = cv2.VideoCapture(get_jetson_gstreamer_source(), cv2.CAP_GSTREAMER)
+
+
+while(video_capture.isOpened()):
+    
+    ret, frame = video_capture.read()
+
+    if (not ret):
+            break
+
+    # crop = frame[800:1000, 600:1920-600]
+    
+    if (predictor3(frame) == 1):
+        dt = datetime.now()
+        dtString = dt_string = dt.strftime("%d/%m/%Y-%H:%M:%S")
+        print(f"Crack detected! {dtString}")
+        cv2.imwrite(f"{os.getcwd()}/Examples/out/{dtString}_crack.jpg", frame)
+
+
+video_capture.release()
